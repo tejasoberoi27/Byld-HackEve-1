647a8e23fe347a3ba59fd482c5da1fac9f18dc4d
Tejas Oberoi <tejas17367@iiitd.ac.in>
2018-01-11 18:11:40 +0530

    changed the (n-3) to (n-2)

--- fib1.py
+++ fib1.py
@@ -1,10 +1,10 @@
 def fib (n):
 	if n < 2:
 		return n
-	return fib(n-3)+fib(n-1)
+	return fib(n-2)+fib(n-1)
 
 
-n = input("Enter the number of times you want to input n :")
+n = int(input("Enter the number of times you want to input n :"))
 for i in range (0,n):
-	a = input ("Enter number :")
-	print fib(a)
+	a = int(input ("Enter number :"))
+	print (fib(a))
